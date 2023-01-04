eacef95da5c1bc48652db950fc9f89b003041bfe
Cléber Severo <clebersevero1998@gmail.com>
2023-01-04 09:38:28 -0300

    fetching the api and showeing the data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,26 @@
-console.log('hello world');
+const button = document.getElementById('btn')
+let coffeItens; 
+
+async function getAPI () {
+    try {
+        const url = await fetch('https://api.sampleapis.com/coffee/hot')
+        let coffeList = await url.json();
+        console.log(coffeList)
+        coffeItens = coffeList;
+    } catch (error) {
+        
+    }
+}
+
+getAPI();
+
+button.addEventListener('click', () => {
+    coffeItens.map((coffe => {
+        console.log(coffe.title);
+    }))
+
+    //showCard();
+})
+
+
+
