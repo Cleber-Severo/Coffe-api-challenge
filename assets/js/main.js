7b98b65323c4c1717c1e00dccea1f93d2e7f0e70
Cléber Severo <clebersevero1998@gmail.com>
2023-01-04 11:50:13 -0300

    creating DOM elements showing the API Data with, title, description and image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 const button = document.getElementById('btn')
+const card = document.querySelector('.card-container');
 let coffeItens; 
 
 async function getAPI () {
@@ -6,20 +7,45 @@ async function getAPI () {
         const url = await fetch('https://api.sampleapis.com/coffee/hot')
         let coffeList = await url.json();
         console.log(coffeList)
+        // console.log(coffeList[0])
+        // console.log(coffeList[0].image)
         coffeItens = coffeList;
+
     } catch (error) {
         
     }
 }
 
+
 getAPI();
 
 button.addEventListener('click', () => {
     coffeItens.map((coffe => {
-        console.log(coffe.title);
+        console.log(coffe);
+        console.log(coffe.image);
+
+        const cardContent = document.createElement('div') 
+        
+        const divTitle = document.createElement('h3') 
+        divTitle.textContent = coffe.title;
+
+        const divDescription = document.createElement('p') 
+        divDescription.textContent = coffe.description;
+
+        const divImg = document.createElement('img')
+        divImg.classList.add('h-40')
+        divImg.setAttribute('src', coffe.image);
+
+        cardContent.appendChild(divTitle)
+        cardContent.appendChild(divDescription)
+        cardContent.appendChild(divImg)
+        console.log(cardContent);
+        card.appendChild(cardContent)
+        
+       
     }))
 
-    //showCard();
+    
 })
 
 
