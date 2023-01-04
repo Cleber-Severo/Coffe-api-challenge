cc6ad5c92eea0479ce999ea559f8fb57fe5ea5f5
Cléber Severo <clebersevero1998@gmail.com>
2023-01-04 19:00:49 -0300

    cleaner code: Separating the tasks by funciton and organizing the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,44 +9,38 @@ async function getAPI () {
         console.log(coffeList)
         coffeItens = coffeList;
 
+        coffeList.map(coffe => showCards(coffe))
+
+        //showCards();
+
     } catch (error) {
         
     }
 }
 
+function showCards (coffe) {
+    const cardContent = document.createElement('div') 
+        
+    const divTitle = document.createElement('h3') 
+    divTitle.textContent = coffe.title;
+
+    const divDescription = document.createElement('p') 
+    divDescription.textContent = coffe.description;
+
+    const divImg = document.createElement('img')
+    divImg.classList.add('h-60', 'w-60')
+    divImg.setAttribute('src', coffe.image);
+
+    cardContent.appendChild(divTitle)
+    cardContent.appendChild(divImg)
+    cardContent.appendChild(divDescription)
+    cardContent.classList.add('p-10', 'bg-gray-200', 'rounded-xl', 'flex','flex-col', 'justfy-center', 'items-center');
+    console.log(cardContent);
+    card.appendChild(cardContent)
+    card.classList.add('p-10');
+    
+}
 
 getAPI();
-
-button.addEventListener('click', () => {
-    coffeItens.map((coffe => {
-        console.log(coffe);
-        console.log(coffe.image);
-
-        const cardContent = document.createElement('div') 
-        
-        const divTitle = document.createElement('h3') 
-        divTitle.textContent = coffe.title;
-
-        const divDescription = document.createElement('p') 
-        divDescription.textContent = coffe.description;
-
-        const divImg = document.createElement('img')
-        divImg.classList.add('h-60', 'w-60')
-        divImg.setAttribute('src', coffe.image);
-
-        cardContent.appendChild(divTitle)
-        cardContent.appendChild(divImg)
-        cardContent.appendChild(divDescription)
-        cardContent.classList.add('p-10', 'bg-gray-200', 'rounded-xl', 'flex','flex-col', 'justfy-center', 'items-center');
-        console.log(cardContent);
-        card.appendChild(cardContent)
-        card.classList.add('p-10');
-        
-       button.style.display = 'none';
-    }))
-
-    
-})
-
 
 
